58bcfb2c27790b86d5daa99c26c8f2de490e3b10
jcLucio <c.lucio3@gmail.com>
2020-08-18 22:26:57 -0500

    Add webpack config files

---
+++ webpack.dev.js
@@ -0,0 +1,23 @@
+const common = require('./webpack.common');
+const merge = require('webpack-merge');
+
+module.exports = merge(common, {
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader', //3. Inject styles into DOM
+          'css-loader', //2. Turns css into commonjs
+          'sass-loader', //1. Turns sass into css
+        ],
+      },
+    ],
+  },
+});
