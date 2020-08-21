ebd0e1fe7d9114852fcd70060e6caa5322ae9088
jcLucio <c.lucio3@gmail.com>
2020-08-21 11:08:58 -0500

    Add no hits test

--- src/app/models/tests/ship.test.js
+++ src/app/models/tests/ship.test.js
@@ -7,4 +7,10 @@ describe('Ship Factory', () => {
     const ship = Ship('battleship');
     expect(ship.length).toBe(4);
   });
+  describe('hit function', () => {
+    test('no hits', () => {
+      const ship = Ship('cruiser');
+      expect(ship.hits).toStrictEqual([null, null, null]);
+    });
+  });
 });
