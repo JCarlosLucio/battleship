0b14578f160b41254b3aba1add9fd45032d698fd
jcLucio <c.lucio3@gmail.com>
2020-08-20 23:25:37 -0500

    Add length to Ship factory

--- src/app/models/factories/ship.js
+++ src/app/models/factories/ship.js
@@ -1,5 +1,16 @@
 const Ship = (type) => {
-  throw new Error('Write tests first');
+  // length
+  const lengths = {
+    carrier: 5,
+    battleship: 4,
+    cruiser: 3,
+    submarine: 3,
+    destroyer: 2,
+  };
+  const length = lengths[type];
+  // hit(num)
+  // isSunk
+  return { length };
 };
 
 export default Ship;
