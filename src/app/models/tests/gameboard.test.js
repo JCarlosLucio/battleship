18068ae60a63d89d76b2f0681b020fa99b53c0d2
jcLucio <c.lucio3@gmail.com>
2020-08-22 16:19:51 -0500

    Add test plan

---
+++ src/app/models/tests/gameboard.test.js
@@ -0,0 +1,7 @@
+// GAMEBOARD
+// place ships at specific coordinates
+// receiveAttack function
+// keep track of missed attacks
+// report whether or not all of their ships have been sunk
+
+import Gameboard from '../factories/gameboard';
