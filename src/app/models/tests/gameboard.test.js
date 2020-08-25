5317f5a01890355f7abc3e196dfabfb91b24426a
jcLucio <c.lucio3@gmail.com>
2020-08-25 18:33:13 -0500

    Add place ship at coord test

--- src/app/models/tests/gameboard.test.js
+++ src/app/models/tests/gameboard.test.js
@@ -5,10 +5,12 @@
 // report whether or not all of their ships have been sunk
 
 import Gameboard from '../factories/gameboard';
+import Ship from '../factories/ship';
 
 describe('Gameboard', () => {
   describe('board', () => {
     const gameboard = Gameboard();
+
     test('empty board', () => {
       const actual = gameboard.getBoard().every((square) => square === null);
       const expected = false;
@@ -23,6 +25,17 @@ describe('Gameboard', () => {
       const actual = gameboard.getBoard()[0].length;
       const expected = 10;
       expect(actual).toBe(expected);
+    });
+  });
+
+  describe('place ship', () => {
+    const gameboard = Gameboard();
+    const ship = Ship('cruiser');
+
+    test('places ship at coord', () => {
+      gameboard.placeShip(ship, 3, 2);
+      const actual = gameboard.getBoard()[3][2];
+      expect(actual).toEqual(ship);
     });
   });
 });
