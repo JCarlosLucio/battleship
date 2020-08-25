386bd9e95f54f3ce73f9dfc26b4bbe0351e2603e
jcLucio <c.lucio3@gmail.com>
2020-08-25 18:31:16 -0500

    Add empty board test

--- src/app/models/tests/gameboard.test.js
+++ src/app/models/tests/gameboard.test.js
@@ -5,3 +5,14 @@
 // report whether or not all of their ships have been sunk
 
 import Gameboard from '../factories/gameboard';
+
+describe('Gameboard', () => {
+  describe('board', () => {
+    const gameboard = Gameboard();
+    test('empty board', () => {
+      const actual = gameboard.getBoard().every((square) => square === null);
+      const expected = false;
+      expect(actual).toBe(expected);
+    });
+  });
+});
