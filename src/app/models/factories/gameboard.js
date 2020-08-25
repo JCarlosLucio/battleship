7134a09b0494637d907c0641d97a274e39d65c2e
jcLucio <c.lucio3@gmail.com>
2020-08-25 18:35:38 -0500

    Add board/getBoard to Gameboard

--- src/app/models/factories/gameboard.js
+++ src/app/models/factories/gameboard.js
@@ -1,5 +1,9 @@
 const Gameboard = () => {
-  throw new Error('Write tests first');
+  // create a board 10x10, coords board[row][col];
+  const board = Array(10).fill(null).map(() => Array(10).fill(null));
+
+  const getBoard = () => board;
+  return { getBoard };
 };
 
 export default Gameboard;
