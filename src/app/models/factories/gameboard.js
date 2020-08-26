c80ee1b090028693da9c490bb8cb6ef2c6110ffc
jcLucio <c.lucio3@gmail.com>
2020-08-26 18:12:37 -0500

    Update placeShip to place ship and index w/direction

--- src/app/models/factories/gameboard.js
+++ src/app/models/factories/gameboard.js
@@ -5,8 +5,19 @@ const Gameboard = () => {
   const getBoard = () => board;
 
   // place ship at coords (y, x)
-  const placeShip = (ship, y, x) => {
-    board[y][x] = ship;
+  const placeShip = (ship, y0, x0) => {
+    const length = ship.length;
+    const direction = ship.getDirection();
+    // todo: check if invalid (out of bounds/collision)
+    if (direction === 'horizontal') {
+      for (let i = 0; i < length; i++) {
+        board[y0][x0 + i] = { ship, index: i };
+      }
+    } else {
+      for (let y = 0; y < length; y++) {
+        board[y0 + i][x0] = { ship, index: y };
+      }
+    }
   };
 
   return { getBoard, placeShip };
