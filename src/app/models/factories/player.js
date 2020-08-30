68495d9e56a897085850cb3a8b13c67caaec7cf9
jcLucio <c.lucio3@gmail.com>
2020-08-29 22:31:02 -0500

    Add attack function

--- src/app/models/factories/player.js
+++ src/app/models/factories/player.js
@@ -1,5 +1,8 @@
 const Player = () => {
-  throw new Error('Write tests first');
+  // Attacks enemyBoard at coords [y][x];
+  const attack = (y, x, enemyBoard) => enemyBoard.receiveAttack(y, x);
+
+  return { attack };
 };
 
 export default Player;
