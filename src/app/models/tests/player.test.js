1ae0b767f11aa4a1d2d5205eafa364e92281ba3d
jcLucio <c.lucio3@gmail.com>
2020-08-29 22:30:39 -0500

    Add Player attack enemy board test

--- src/app/models/tests/player.test.js
+++ src/app/models/tests/player.test.js
@@ -1,1 +1,16 @@
 import Player from '../factories/player';
+import Gameboard from '../factories/gameboard';
+
+// Setup
+const player = Player();
+const enemy = Player();
+const playerBoard = Gameboard();
+const enemyBoard = Gameboard();
+
+describe('Player attack', () => {
+  test('attack enemy board', () => {
+    player.attack(2, 3, enemyBoard);
+    const actual = enemyBoard.getBoard()[2][3];
+    expect(actual).toBe('miss');
+  });
+});
