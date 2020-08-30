653aab2412783fccf1d1376b1c78ad943e1972b8
jcLucio <c.lucio3@gmail.com>
2020-08-30 17:04:19 -0500

    Add autoAttack function

--- src/app/models/factories/player.js
+++ src/app/models/factories/player.js
@@ -2,7 +2,21 @@ const Player = () => {
   // Attacks enemyBoard at coords [y][x];
   const attack = (y, x, enemyBoard) => enemyBoard.receiveAttack(y, x);
 
-  return { attack };
+  const autoAttack = (enemyBoard) => {
+    const rand = () => Math.floor(Math.random() * 10);
+    const y = rand();
+    const x = rand();
+    const cell = enemyBoard.getBoard()[y][x];
+    if (cell === 'miss' || cell === 'hit') {
+      // tries again until it valid cell is picked
+      autoAttack(enemyBoard);
+    } else {
+      // attacks a valid cell
+      enemyBoard.receiveAttack(y, x);
+    }
+  };
+
+  return { attack, autoAttack };
 };
 
 export default Player;
