5a9c11669fcc1a206e039b4b47c6e6d67a9721bb
jcLucio <c.lucio3@gmail.com>
2020-09-03 22:48:49 -0500

    Add renderGrid

--- src/app/views/gameboardView.js
+++ src/app/views/gameboardView.js
@@ -1,2 +1,12 @@
 const renderCell = (y, x) =>
   `<div class="grid-cell cell-${y}-${x}" data-y='${y}' data-x='${x}'></div>`;
+
+export const renderGrid = (size, parent) => {
+  let grid = '';
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      grid += renderCell(i, j);
+    }
+  }
+  parent.insertAdjacentHTML('afterbegin', grid);
+};
