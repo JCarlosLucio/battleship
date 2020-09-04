595fa0a0eb9b4ec174ce1128259b28c2369b7599
jcLucio <c.lucio3@gmail.com>
2020-09-04 14:37:08 -0500

    Add Player type tests

--- src/app/models/tests/player.test.js
+++ src/app/models/tests/player.test.js
@@ -2,10 +2,21 @@ import Player from '../factories/player';
 import Gameboard from '../factories/gameboard';
 
 // Setup
-const player = Player();
-const enemy = Player();
+const player = Player('human');
+const enemy = Player('computer');
 const playerBoard = Gameboard();
 const enemyBoard = Gameboard();
+
+describe('Player type', () => {
+  test('human type', () => {
+    const actual = player.getType();
+    expect(actual).toBe('human');
+  });
+  test('computer type', () => {
+    const actual = enemy.getType();
+    expect(actual).toBe('computer');
+  });
+});
 
 describe('Player attack', () => {
   test('attack enemy board', () => {
@@ -18,7 +29,6 @@ describe('Player attack', () => {
 describe('Auto attack', () => {
   test('attack enemy board', () => {
     enemy.autoAttack(playerBoard);
-
     const actual = playerBoard.getBoard().flat().every((cell) => cell === null);
     expect(actual).toBe(false);
   });
