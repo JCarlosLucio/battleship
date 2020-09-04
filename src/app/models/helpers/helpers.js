26fc6c965e3b66d927277dbf306cdef2a99e85ed
jcLucio <c.lucio3@gmail.com>
2020-09-04 15:44:00 -0500

    Add createFleet to helpers

--- src/app/models/helpers/helpers.js
+++ src/app/models/helpers/helpers.js
@@ -19,3 +19,11 @@ export const SHIP_LENGTHS = {
 const rand = (size = 10) => Math.floor(Math.random() * size);
 
 export const randCoords = (size = 10) => [rand(size), rand(size)];
+
+export const createFleet = (types) => {
+  // creates an object of Ships
+  // fleet = {carrier: {id: "carrier", length: 5, ...}, ...otherShips}
+  const fleet = {};
+  types.forEach((type) => (fleet[type] = Ship(type)));
+  return fleet;
+};
