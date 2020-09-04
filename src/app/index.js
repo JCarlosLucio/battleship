254317836ac4a8da12d886d175c901bac867bb55
jcLucio <c.lucio3@gmail.com>
2020-09-04 14:38:10 -0500

    Update players w/type

--- src/app/index.js
+++ src/app/index.js
@@ -6,8 +6,8 @@ import '../styles/main.scss';
 
 // INIT
 // 1. Create Players/Boards
-const p1 = Player();
-const p2 = Player();
+const p1 = Player('human');
+const p2 = Player('computer');
 
 const p1Board = Gameboard();
 const p2Board = Gameboard();
@@ -15,4 +15,5 @@ const p2Board = Gameboard();
 // 2. Render Grids
 gameboardView.renderGrid(elements.p1Grid);
 gameboardView.renderGrid(elements.p2Grid);
+
 // 3. Place Ships
