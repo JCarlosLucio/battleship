bf3e843fb3b6a709f9a0b4adfe43c7688ab7c1ac
jcLucio <c.lucio3@gmail.com>
2020-09-04 12:33:35 -0500

    Create Players p1/p2

--- src/app/index.js
+++ src/app/index.js
@@ -1,3 +1,7 @@
 import Gameboard from './models/factories/gameboard';
 import Player from './models/factories/player';
 import '../styles/main.scss';
+
+// Init
+const p1 = Player();
+const p2 = Player();
