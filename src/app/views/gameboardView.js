9029b134b1ef882164184fe0ab385db13d7a0f02
jcLucio <c.lucio3@gmail.com>
2020-09-04 14:20:21 -0500

    Refactor gameboardView

--- src/app/views/gameboardView.js
+++ src/app/views/gameboardView.js
@@ -1,12 +1,18 @@
-const renderCell = (y, x) =>
-  `<div class="grid-cell cell-${y}-${x}" data-y='${y}' data-x='${x}'></div>`;
+const gameboardView = (() => {
+  const renderCell = (y, x) =>
+    `<div class="grid-cell cell-${y}-${x}" data-y='${y}' data-x='${x}'></div>`;
 
-export const renderGrid = (size, parent) => {
-  let grid = '';
-  for (let i = 0; i < size; i++) {
-    for (let j = 0; j < size; j++) {
-      grid += renderCell(i, j);
+  const renderGrid = (parent, size = 10) => {
+    let grid = '';
+    for (let i = 0; i < size; i++) {
+      for (let j = 0; j < size; j++) {
+        grid += renderCell(i, j);
+      }
     }
-  }
-  parent.insertAdjacentHTML('afterbegin', grid);
-};
+    parent.insertAdjacentHTML('afterbegin', grid);
+  };
+
+  return { renderGrid };
+})();
+
+export default gameboardView;
