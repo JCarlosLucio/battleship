dc757f8850ce6eabacb2c39aebbfbe0ecec2385f
jcLucio <c.lucio3@gmail.com>
2020-09-04 15:39:58 -0500

    Add constants to helpers

---
+++ src/app/models/helpers/helpers.js
@@ -0,0 +1,16 @@
+// CONSTANTS
+export const SHIP_TYPES = [
+  'carrier',
+  'battleship',
+  'cruiser',
+  'submarine',
+  'destroyer',
+];
+
+export const SHIP_LENGTHS = {
+  carrier: 5,
+  battleship: 4,
+  cruiser: 3,
+  submarine: 3,
+  destroyer: 2,
+};
