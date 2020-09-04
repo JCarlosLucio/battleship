52260c848bc2700ac3f4caeb9e47a5c6a14d102a
jcLucio <c.lucio3@gmail.com>
2020-09-04 14:21:34 -0500

    Import gameboardView/elements

--- src/app/index.js
+++ src/app/index.js
@@ -1,10 +1,16 @@
 import Gameboard from './models/factories/gameboard';
 import Player from './models/factories/player';
+import gameboardView from './views/gameboardView';
+import { elements } from './views/base';
 import '../styles/main.scss';
 
-// Init
+// INIT
+// 1. Create Players/Boards
 const p1 = Player();
 const p2 = Player();
 
 const p1Board = Gameboard();
 const p2Board = Gameboard();
+
+// 2. Render Grids
+// 3. Place Ships
