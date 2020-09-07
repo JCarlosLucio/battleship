3e3acef4ca966681908c96a1c49ce6f771a9db1c
jcLucio <c.lucio3@gmail.com>
2020-09-06 22:43:22 -0500

    Update renderGrid/renderCell

--- src/app/views/gameboardView.js
+++ src/app/views/gameboardView.js
@@ -1,12 +1,20 @@
 const gameboardView = (() => {
-  const renderCell = (y, x) =>
-    `<div class="grid-cell cell-${y}-${x}" data-y='${y}' data-x='${x}'></div>`;
+  const renderCell = (y, x, status) =>
+    `<div class="grid-cell cell-${y}-${x} ${status}" data-y='${y}' data-x='${x}'></div>`;
 
-  const renderGrid = (parent, size = 10) => {
+  const renderGrid = (parent, gameboard) => {
+    const board = gameboard.getBoard();
+    const length = board.length;
     let grid = '';
-    for (let i = 0; i < size; i++) {
-      for (let j = 0; j < size; j++) {
-        grid += renderCell(i, j);
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length; j++) {
+        let status = board[i][j];
+        if (status === null) {
+          status = '';
+        } else if (status.ship) {
+          status = 'placed';
+        }
+        grid += renderCell(i, j, status);
       }
     }
     parent.insertAdjacentHTML('afterbegin', grid);
