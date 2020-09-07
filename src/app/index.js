43073be711c81985cf53af91d3d478c8e8276319
jcLucio <c.lucio3@gmail.com>
2020-09-06 22:53:14 -0500

    Add update grid w/placed ships

--- src/app/index.js
+++ src/app/index.js
@@ -12,12 +12,14 @@ const p2 = Player('computer');
 const p1Board = Gameboard();
 const p2Board = Gameboard();
 
-// 2. Render Grids
-gameboardView.renderGrid(elements.p1Grid);
-gameboardView.renderGrid(elements.p2Grid);
+// 2. Render Empty Grids
+// gameboardView.renderGrid(elements.p1Grid, p1Board);
+// gameboardView.renderGrid(elements.p2Grid, p2Board);
 
 // 3. Place Ships... for now autoPlaceFleet (later button and/or drag-n-drop)
 p1Board.autoPlaceFleet(p1.getFleet());
 p2Board.autoPlaceFleet(p2.getFleet());
 
-// 4. Show placed Ships in grid
+// 4. Update grid w/placed Ships (later only show player board if vs 'computer')
+gameboardView.renderGrid(elements.p1Grid, p1Board);
+gameboardView.renderGrid(elements.p2Grid, p2Board);
