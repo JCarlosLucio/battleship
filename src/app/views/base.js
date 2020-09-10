720b2c31e142577e8fc49d9cca0007eab2a5258d
jcLucio <c.lucio3@gmail.com>
2020-09-10 15:14:52 -0500

    Add autoPlaceBtn to elements

--- src/app/views/base.js
+++ src/app/views/base.js
@@ -2,4 +2,5 @@ export const elements = {
   p1Grid: document.querySelector('.p1-grid'),
   p2Grid: document.querySelector('.p2-grid'),
   infoText: document.querySelector('.info-text'),
+  autoPlaceBtn: document.querySelector('.fleet-autoplace'),
 };
