131a5370d759abac216b529482a06e7b22e54e0a
jcLucio <c.lucio3@gmail.com>
2020-09-10 10:37:29 -0500

    Update winnerText to infoText

--- src/app/views/base.js
+++ src/app/views/base.js
@@ -1,5 +1,5 @@
 export const elements = {
   p1Grid: document.querySelector('.p1-grid'),
   p2Grid: document.querySelector('.p2-grid'),
-  winnerText: document.querySelector('.winner-text'),
+  infoText: document.querySelector('.info-text'),
 };
