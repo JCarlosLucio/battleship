62d633c8aaa689760459d432a6bc543145438079
jcLucio <c.lucio3@gmail.com>
2020-09-10 12:34:06 -0500

    Add condition to ctrlAttack to avoid 'border hits'

--- src/app/index.js
+++ src/app/index.js
@@ -27,35 +27,37 @@ gameboardView.renderGrid(elements.p2Grid, p2Board, p2.getType());
 // 5. ctrlAttack function for eventListeners
 const ctrlAttack = (e) => {
   const cell = e.target;
-  // 1. Get coords from cell
-  const y = cell.dataset.y;
-  const x = cell.dataset.x;
+  if (cell.classList.contains('grid-cell')) {
+    // 1. Get coords from cell
+    const y = cell.dataset.y;
+    const x = cell.dataset.x;
 
-  // 2. Checks that board cell hasn't been attacked
-  const boardCell = p2Board.getBoard()[y][x];
-  if (boardCell !== 'miss' && boardCell !== 'hit') {
-    // 3. Makes Attacks for p1 'human' and p2 'computer'
-    p1.attack(y, x, p2Board);
-    p2.autoAttack(p1Board);
+    // 2. Checks that board cell hasn't been attacked
+    const boardCell = p2Board.getBoard()[y][x];
+    if (boardCell !== 'miss' && boardCell !== 'hit') {
+      // 3. Makes Attacks for p1 'human' and p2 'computer'
+      p1.attack(y, x, p2Board);
+      p2.autoAttack(p1Board);
 
-    // 4. Updates grids after attacks to show outcome
-    gameboardView.renderGrid(elements.p1Grid, p1Board, p1.getType());
-    gameboardView.renderGrid(elements.p2Grid, p2Board, p2.getType());
-  }
-
-  // 5. Checks if all ships are sunk
-  if (p1Board.areShipsSunk() || p2Board.areShipsSunk()) {
-    let winner = '';
-    if (p1Board.areShipsSunk()) {
-      winner = 'Player 2';
-    } else if (p2Board.areShipsSunk()) {
-      winner = 'Player 1';
+      // 4. Updates grids after attacks to show outcome
+      gameboardView.renderGrid(elements.p1Grid, p1Board, p1.getType());
+      gameboardView.renderGrid(elements.p2Grid, p2Board, p2.getType());
     }
-    // 6. Disable eventListeners for attacks
-    elements.p2Grid.removeEventListener('click', ctrlAttack);
-    // 7. Display WiNNER
-    gameboardView.renderWinner(winner);
-    // 8. Display 'Play Again? Button'
+
+    // 5. Checks if all ships are sunk
+    if (p1Board.areShipsSunk() || p2Board.areShipsSunk()) {
+      let winner = '';
+      if (p1Board.areShipsSunk()) {
+        winner = 'Player 2';
+      } else if (p2Board.areShipsSunk()) {
+        winner = 'Player 1';
+      }
+      // 6. Disable eventListeners for attacks
+      elements.p2Grid.removeEventListener('click', ctrlAttack);
+      // 7. Display WiNNER
+      gameboardView.renderWinner(winner);
+      // 8. Display 'Play Again? Button'
+    }
   }
 };
 
