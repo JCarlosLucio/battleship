5aec290a314a4faa68cd0991f4e125ff0ef18f84
jcLucio <c.lucio3@gmail.com>
2020-09-12 13:57:13 -0500

    Add infoContainer to elements

--- src/app/views/base.js
+++ src/app/views/base.js
@@ -1,6 +1,7 @@
 export const elements = {
   p1Grid: document.querySelector('.p1-grid'),
   p2Grid: document.querySelector('.p2-grid'),
+  infoContainer: document.querySelector('.info-container'),
   infoText: document.querySelector('.info-text'),
   autoPlaceBtn: document.querySelector('.fleet-autoplace'),
 };
