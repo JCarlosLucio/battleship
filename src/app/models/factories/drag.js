33fbb1b4db1369a8eb93afd6f23e310e23c025d1
jcLucio <c.lucio3@gmail.com>
2020-09-20 13:03:58 -0500

    Refactored drag-n-drop logic to drag.js

---
+++ src/app/models/factories/drag.js
@@ -0,0 +1,79 @@
+import { elements } from '../../views/base';
+import gameboardView from '../../views/gameboardView';
+
+const Drag = (p1, p1Board) => {
+  let draggedShip;
+  let draggedShipIndex;
+  // console.log(draggedShip);
+
+  //  Drag-N-Drop (for now for p1 only)
+  const getDraggedShipIndex = (e) => {
+    draggedShipIndex = e.target.dataset.index;
+    // console.log(draggedShipIndex);
+  };
+
+  const dragStart = (e) => {
+    draggedShip = e.target;
+    console.log('start', { draggedShip });
+  };
+  const dragOver = (e) => {
+    e.preventDefault();
+    // console.log('Over');
+  };
+  const dragEnter = (e) => {
+    e.preventDefault();
+    // console.log('Enter');
+  };
+  const dragLeave = (e) => {
+    // console.log('Leave');
+  };
+  const dragDrop = (e) => {
+    // get cell
+    const cell = e.target;
+    // get coords - adjust coords according to draggedShipIndex
+    // todo - adjust coords for direction
+    const y = Number(cell.dataset.y);
+    const x = Number(cell.dataset.x) - Number(draggedShipIndex);
+
+    // get ship
+    const p1Ship = p1.getFleet()[draggedShip.dataset.ship];
+    console.log('DROP', draggedShip, p1Ship, { x }, { y });
+    // place ship and get outcome
+    const outcome = p1Board.placeShip(p1Ship, y, x);
+    console.log(outcome);
+    if (outcome) {
+      // update grid
+      gameboardView.renderGrid(elements.p1Grid, p1Board, p1.getType());
+      addDragAndDropEvenListeners();
+      // remove ship from fleet container
+      elements.fleetContainer.removeChild(draggedShip);
+      // todo - show START button after last ship has been placed
+      // todo - enemy autoPlaceFleet
+    }
+  };
+  const dragEnd = (e) => {
+    // console.log('End');
+  };
+
+  const addDragAndDropEvenListeners = () => {
+    const ships = document.querySelectorAll('.ship');
+    const cells = elements.p1Grid.childNodes;
+
+    // Add EventListener to know which index is being held when dragging
+    // Add EventListners for drag/drop events
+    for (const ship of ships) {
+      ship.addEventListener('mousedown', getDraggedShipIndex);
+      ship.addEventListener('dragstart', dragStart);
+      ship.addEventListener('dragend', dragEnd);
+    }
+    for (const cell of cells) {
+      cell.addEventListener('dragover', dragOver);
+      cell.addEventListener('dragenter', dragEnter);
+      cell.addEventListener('dragleave', dragLeave);
+      cell.addEventListener('drop', dragDrop);
+    }
+  };
+  return { addDragAndDropEvenListeners };
+};
+
+export default Drag;
--- src/app/models/factories/game.js
+++ src/app/models/factories/game.js
@@ -35,78 +35,6 @@ const Game = (type) => {
     addDragAndDropEvenListeners();
   };
 
-  //  ===== Drag N Drop - (for now for p1 only) ===
-  const addDragAndDropEvenListeners = () => {
-    const ships = document.querySelectorAll('.ship');
-    const cells = elements.p1Grid.childNodes;
-
-    // Add EventListener to know which index is being held when dragging
-    // Add EventListners for drag/drop events
-    for (const ship of ships) {
-      ship.addEventListener('mousedown', getDraggedShipIndex);
-      ship.addEventListener('dragstart', dragStart);
-      ship.addEventListener('dragend', dragEnd);
-    }
-    for (const cell of cells) {
-      cell.addEventListener('dragover', dragOver);
-      cell.addEventListener('dragenter', dragEnter);
-      cell.addEventListener('dragleave', dragLeave);
-      cell.addEventListener('drop', dragDrop);
-    }
-  };
-
-  let draggedShip;
-  let draggedShipIndex;
-  // console.log(draggedShip);
-
-  const getDraggedShipIndex = (e) => {
-    draggedShipIndex = e.target.dataset.index;
-    // console.log(draggedShipIndex);
-  };
-
-  const dragStart = (e) => {
-    draggedShip = e.target;
-    console.log('start', { draggedShip });
-  };
-  const dragOver = (e) => {
-    e.preventDefault();
-    // console.log('Over');
-  };
-  const dragEnter = (e) => {
-    e.preventDefault();
-    // console.log('Enter');
-  };
-  const dragLeave = (e) => {
-    // console.log('Leave');
-  };
-  const dragDrop = (e) => {
-    // get cell
-    const cell = e.target;
-    // get coords - adjust coords according to draggedShipIndex
-    // todo - adjust coords for direction
-    const y = Number(cell.dataset.y);
-    const x = Number(cell.dataset.x) - Number(draggedShipIndex);
-    // get ship
-    const p1Ship = p1.getFleet()[draggedShip.dataset.ship];
-    console.log('DROP', draggedShip, p1Ship, { x }, { y });
-    // place ship and get outcome
-    const outcome = p1Board.placeShip(p1Ship, y, x);
-    console.log(outcome);
-    if (outcome) {
-      // update grid
-      gameboardView.renderGrid(elements.p1Grid, p1Board, p1.getType());
-      addDragAndDropEvenListeners();
-      // remove ship from fleet container
-      elements.fleetContainer.removeChild(draggedShip);
-      // todo - show START button after last ship has been placed
-      // todo - enemy autoPlaceFleet
-    }
-  };
-  const dragEnd = (e) => {
-    // console.log('End');
-  };
-  // ==== Ends Drag-n-Drop ====
-
   //  EventListener for p1 'human' player
   const addGridEventListeners = () => {
     if (p2.getType === 'human')
@@ -177,7 +105,6 @@ const Game = (type) => {
   return {
     renderGrids,
     renderFleet,
-    addDragAndDropEvenListeners,
     autoPlace,
     startGame,
     playAgain,
